7ad388132aae49843677d8331574b5a81a06e251
kenro-t <ngkn1425@gmail.com>
2023-12-19 23:58:30 +0900

    関数

---
+++ a_tour_of_go_2.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+// 引数の型は同じ場合まとめて定義できる
+func add(x, y int) int {
+	return x + y
+}
+
+// 個別と複数で共存可能
+func add2(x, y int, z string) int {
+	return x + y
+}
+
+// 戻り値は複数設定可能
+func multiReturn(x, y int) (int, int) {
+	return x , y
+}
+
+func main () {
+	fmt.Println(add(123, 321))
+	fmt.Println(add2(123, 321, "111"))
+	fmt.Println(multiReturn(123, 321))
+}
