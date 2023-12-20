b43a7615137abe62d1dce4103bff0959cf86214d
kenro-t <ngkn1425@gmail.com>
2023-12-20 21:10:26 +0900

    変数と定数

---
+++ a_tour_of_go_3.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// 関数の外では省略形で書くことができない
+var i, j int = 1, 2
+
+// 初期値を与えずに宣言するとゼロ値が付与される
+// var i int
+var f float64
+var b bool
+var s string
+func main () {
+	// 関数内であれば暗黙な型宣言を利用できる
+	// k := 3
+
+	// 定数は省略形では書けない
+	const hoge = "aaa"
+	fmt.Println(hoge)
+}
