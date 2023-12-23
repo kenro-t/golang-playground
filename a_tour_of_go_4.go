3b687fde7b2ac1f3f8b1f067180d580bc0ec147f
kenro-t <ngkn1425@gmail.com>
2023-12-24 01:12:39 +0900

    wordCount

---
+++ a_tour_of_go_4.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"golang.org/x/tour/wc"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	maps := map[string]int{}
+	words := strings.Split(s, " ")
+	
+	for _, v := range words {
+		count := 0
+		for _, v2 := range words {
+			if v == v2 {
+				count++
+			}
+		}
+		maps[v] = count
+	}
+	return maps
+}
+
+func main() {
+	wc.Test(WordCount)
+}
