1eeba1fa4bb8a3c25a3f856ca69b75c09d305306
kenro-t <ngkn1425@gmail.com>
2025-01-05 14:12:45 +0000

    途中まで

--- go_todo_app/main.go
+++ go_todo_app/main.go
@@ -1,18 +1,31 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
-	err := http.ListenAndServe(
-		":80",
-		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	if err := run(context.Background()); err != nil {
+		log.Printf("Error: %v", err)
+	}
+
+	// if err != nil {
+	// 	fmt.Printf("failed to start server: %v", err)
+	// }
+}
+
+func run(ctx context.Context) error {
+	s := http.Server{
+		Addr: ":80",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Hello %s, World!", r.URL.Path[1:])
 		}),
-	)
-	if err != nil {
-		fmt.Printf("failed to start server: %v", err)
 	}
+
+	s.ListenAndServe()
+
+	return nil
 }
