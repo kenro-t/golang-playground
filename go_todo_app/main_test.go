a528f4e359d69b055ae95edc38f4f5dce61749b1
kenro-t <ngkn1425@gmail.com>
2025-01-10 14:30:25 +0000

    テストコード調整

--- go_todo_app/main_test.go
+++ go_todo_app/main_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"net"
 	"net/http"
 	"testing"
 
@@ -11,14 +12,21 @@ import (
 )
 
 func TestRun(t *testing.T) {
+	l, err := net.Listen("tcp", "localhost:0")
+	if err != nil {
+		t.Fatalf("failed to listen %v", err)
+	}
 	ctx, cancel := context.WithCancel(context.Background())
 	eg, ctx := errgroup.WithContext(ctx)
 	eg.Go(func() error {
-		return run(ctx)
+		return run(ctx, l)
 	})
 
 	in := "message"
-	resp, err := http.Get("http://localhost:80/" + in)
+	url := fmt.Sprintf("http://%s/%s", l.Addr().String(), in)
+	t.Logf("try request to: %s", url)
+
+	resp, err := http.Get(url)
 	if err != nil {
 		t.Errorf("failed to get %v", err)
 	}
