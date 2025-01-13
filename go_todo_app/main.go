e86e101c43eeb0daa8a4fb8c03c36ddcd132db18
kenro-t <ngkn1425@gmail.com>
2025-01-13 14:01:39 +0000

    終了シグナルの割り込みとか

--- go_todo_app/main.go
+++ go_todo_app/main.go
@@ -3,38 +3,50 @@ package main
 import (
 	"context"
 	"fmt"
+	"go_todo_app/config"
 	"log"
 	"net"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"golang.org/x/sync/errgroup"
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		log.Printf("need port number")
-	}
-
-	// ポート番号を引数として取得
-	p := os.Args[1]
-	l, err := net.Listen("tcp", ":"+p)
-	if err != nil {
-		log.Printf("failed to listen %v", err)
-	}
-
-	if err := run(context.Background(), l); err != nil {
+	if err := run(context.Background()); err != nil {
 		log.Printf("Error: %v", err)
 	}
 }
 
-func run(ctx context.Context, l net.Listener) error {
+func run(ctx context.Context) error {
+	// グレースフルシャットダウンのためのコンテキスト
+	// 割り込み（Interrupt）があったときに終了（SIGTERM）を実行する
+	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	// 設定ファイルを読み込む
+	cfg, err := config.New()
+	if err != nil {
+		return err
+	}
+
+	// ポートをリッスンする
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port))
+	if err != nil {
+		log.Fatalf("failed to listen port %d, %v", cfg.Port, err)
+	}
+
 	s := http.Server{
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			time.Sleep(5 * time.Second)
 			fmt.Fprintf(w, "Hello %s, World!", r.URL.Path[1:])
 		}),
 	}
 
+	// errgroupでサーバーを起動する（errgroupは通常のゴルーチンと違いエラーを返してくれる）
 	eg, ctx := errgroup.WithContext(ctx)
 
 	eg.Go(func() error {
@@ -45,6 +57,7 @@ func run(ctx context.Context, l net.Listener) error {
 		return nil
 	})
 
+	// シグナルを受け取ったらシャットダウンする
 	<-ctx.Done()
 	if err := s.Shutdown(context.Background()); err != nil {
 		log.Printf("failed to shutdown %v", err)
