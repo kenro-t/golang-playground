d7857f0ead4d36ef34c7d415b54daa923fc1cdf7
kenro-t <ngkn1425@gmail.com>
2025-01-18 12:59:12 +0000

    muxテストコードとリファクタリング

--- go_todo_app/main.go
+++ go_todo_app/main.go
@@ -6,25 +6,29 @@ import (
 	"go_todo_app/config"
 	"log"
 	"net"
-	"net/http"
 )
 
 func main() {
+	ctx := context.Background()
+	if err := run(ctx); err != nil {
+		log.Printf("Error: %v", err)
+	}
+}
+
+func run(ctx context.Context) error {
 	cfg, err := config.New()
 	if err != nil {
-		log.Printf("Error: %v", err)
+		return err
 	}
-
 	l, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", cfg.Port))
 	if err != nil {
-		log.Printf("Error: %v", err)
+		return err
 	}
-	mux := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello %s, World!", r.URL.Path[1:])
-	})
+	url := fmt.Sprintf("http://%s", l.Addr().String())
+	log.Printf("Server start with %s", url)
 
+	mux := NewMux()
 	s := NewServer(l, mux)
-	if err := s.Run(context.Background()); err != nil {
-		log.Printf("Error: %v", err)
-	}
+
+	return s.Run(ctx)
 }
